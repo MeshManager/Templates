5b3a8c4efa1abc3c2a2a028a726ab65f6e25d709
S6John <john_sungho@naver.com>
2025-06-23 11:33:32 +0900

    fix: kind 추가

--- eks/eks-nodegroup.bash
+++ eks/eks-nodegroup.bash
@@ -2,17 +2,12 @@
 
 export CLUSTER_NAME="mesh-manager-eks"
 
+eksctl create nodegroup --config-file=./eks-nodegroup.yaml
+
 echo "기존 노드그룹 목록:"
 eksctl get nodegroups --cluster=$CLUSTER_NAME --output=json | jq -r '.[]'
 
 read -p "삭제할 노드그룹 이름을 입력하세요: " OLD_NODEGROUP
-
-
-
-
-
-
-eksctl create nodegroup --config-file=./eks.yaml
 
 kubectl taint nodes -l "eks.amazonaws.com/nodegroup=$OLD_NODEGROUP" key=value:NoExecute
 
