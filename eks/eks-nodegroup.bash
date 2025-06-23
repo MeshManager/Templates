b67f8a9df0bcc052a1b6d8758cd4db35062ec87a
S6John <john_sungho@naver.com>
2025-06-23 11:30:19 +0900

    fix: node group 바꾸는 부분 수정

---
+++ eks/eks-nodegroup.bash
@@ -0,0 +1,22 @@
+#! /bin/bash
+
+export CLUSTER_NAME="mesh-manager-eks"
+
+echo "기존 노드그룹 목록:"
+eksctl get nodegroups --cluster=$CLUSTER_NAME --output=json | jq -r '.[]'
+
+read -p "삭제할 노드그룹 이름을 입력하세요: " OLD_NODEGROUP
+
+
+
+
+
+
+eksctl create nodegroup --config-file=./eks.yaml
+
+kubectl taint nodes -l "eks.amazonaws.com/nodegroup=$OLD_NODEGROUP" key=value:NoExecute
+
+eksctl drain nodegroup --cluster $CLUSTER_NAME --name $OLD_NODEGROUP
+
+
+eksctl delete nodegroup --cluster $CLUSTER_NAME --name $OLD_NODEGROUP
