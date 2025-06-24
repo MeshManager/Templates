55abb1cf4276c580dc0f87c2ea544aad3c3b99ce
Sungho, Kim <john_sungho@naver.com>
2025-06-25 00:25:39 +0900

    feat: MED 제거

--- cloudFront/securityHub/alarm-chain/chain.bash
+++ cloudFront/securityHub/alarm-chain/chain.bash
@@ -7,4 +7,5 @@ read -p "slack web Hook URL을 입력하세요: " PARAM_URL
 aws cloudformation create-stack \
   --stack-name security-hub-alarm-chain \
   --template-body file://$(pwd)/chain.yaml \
-  --parameters ParameterKey=Url,ParameterValue=${PARAM_URL}
+  --parameters ParameterKey=SlackWebhookUrl,ParameterValue=${PARAM_URL} \
+  --capabilities CAPABILITY_NAMED_IAM
