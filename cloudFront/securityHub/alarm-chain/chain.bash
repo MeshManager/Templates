2b98cd0d546d612fa5258e95373bd2ae4e4ea2a0
Sungho, Kim <john_sungho@naver.com>
2025-06-25 00:15:34 +0900

    feat: add security hub bash

---
+++ cloudFront/securityHub/alarm-chain/chain.bash
@@ -0,0 +1,10 @@
+#! /bin/bash
+
+# 파라미터 값 변수에 할당
+read -p "slack web Hook URL을 입력하세요: " PARAM_URL
+
+# CloudFormation 스택 생성
+aws cloudformation create-stack \
+  --stack-name your-stack-name \
+  --template-body file://$(pwd)/chain.yaml \
+  --parameters ParameterKey=Url,ParameterValue=${PARAM_URL}
